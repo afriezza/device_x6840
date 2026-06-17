157e4902eef5a25abb55dd05241464525cf3c378
afriezza <wongjen07@gmail.com>
2026-06-17 10:27:36 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,25 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit dari konfigurasi dasar (PENTING: Harus ada)
+# Inherit dari konfigurasi dasar
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Memanggil folder root recovery bawaan ramdisk asli
 PRODUCT_PACKAGES += \
-    otacerts
-
-# Mengaktifkan update dynamic partitions saat di recovery
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# --- TAMBAHAN WAJIB UNTUK MEDIATEK ---
-# Agar TWRP bisa membaca partisi super/dynamic
-PRODUCT_PACKAGES += \
+    otacerts \
     fastbootd
 
-# Flag tambahan agar boot image header terbaca dengan benar
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.boot.header_version=4
+# Mengaktifkan update dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Identitas build
 PRODUCT_DEVICE := X6840
@@ -31,7 +23,6 @@ PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix SMART 20
 PRODUCT_MANUFACTURER := Infinix
 
-# Tambahkan ini ke device.mk lo
+# Property Overrides (Digabung biar rapi)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.header_version=4
-
