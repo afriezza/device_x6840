57227a97e98f8f60291658f33525d4f93472a44d
afriezza <wongjen07@gmail.com>
2026-06-17 10:21:33 +0700

    Modify product name and configuration references
    Updated product name from 'twrp_X6840' to 'omni_X6840' and removed references to OrangeFox configuration.

--- twrp_X6840.mk
+++ twrp_X6840.mk
@@ -1,17 +1,9 @@
-# SPDX-License-Identifier: Apache-2.0
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# MANDRA UTAMA: Panggil konfigurasi resmi milik OrangeFox
-$(call inherit-product, vendor/fox/config/fox.mk)
-
-# Menunjuk ke repo ini sendiri
-$(call inherit-product, device/infinix/X6840/device.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := X6840
-PRODUCT_NAME := twrp_X6840
+PRODUCT_NAME := omni_X6840
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix SMART 20
 PRODUCT_MANUFACTURER := Infinix
-PRODUCT_GMS_CLIENTID_BASE := android-infinix
